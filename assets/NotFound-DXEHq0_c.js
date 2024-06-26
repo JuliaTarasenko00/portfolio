6ea9715a6893402fb715ad22ac022b6afbe8a000
JuliaTarasenko00 <118853187+JuliaTarasenko00@users.noreply.github.com>
2024-06-26 11:41:15 +0000

    Deploying to gh-pages from @ JuliaTarasenko00/portfolio@aec0754722db9d8ad0baebb4c98fe3d290c13e36 🚀

--- assets/NotFound-DXEHq0_c.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BYy1NT7f.js";function n(){return o.jsx("p",{children:"NotFound"})}export{n as default};
--- assets/Projects-vKaowMCH.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BYy1NT7f.js";function e(){return r.jsx("p",{children:"Projects"})}export{e as default};
