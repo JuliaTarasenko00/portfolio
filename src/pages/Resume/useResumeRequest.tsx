46d51a785188db6a6f8382347d059903b27aa4d5
JuliaTarasenko00 <118853187+JuliaTarasenko00@users.noreply.github.com>
2024-07-09 20:57:09 +0300

    fix

--- src/pages/Resume/useResumeRequest.tsx
+++ src/pages/Resume/useResumeRequest.tsx
@@ -1,14 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { useLanguage } from '../../helpers/context/useLanguage';
 import { getEducation } from '../../api/education';
-import { IEducation } from '../../types/typeResume';
 
 export const useResumeRequest = () => {
   const { currentLanguage: language } = useLanguage();
 
-  const { data: educationList, isPending: isPendingEducation } = useQuery<
-    IEducation[]
-  >({
+  const { data: educationList, isPending: isPendingEducation } = useQuery({
     queryKey: ['education', language],
     queryFn: () => getEducation(language),
   });
