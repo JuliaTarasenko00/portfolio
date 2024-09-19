a9be0f91a7a7fb69a10d044620269b243bb548ee
JuliaTarasenko00 <118853187+JuliaTarasenko00@users.noreply.github.com>
2024-09-19 18:58:17 +0000

    Deploying to gh-pages from @ JuliaTarasenko00/portfolio@fac9f2869b4d844eaa9ba693362857bb359950b3 🚀

---
+++ assets/useMainInformation-BG2mzvXV.js
@@ -0,0 +1,1 @@
+import{r as n,M as o}from"./index-B8HGPGj2.js";const e=()=>{const t=n.useContext(o);if(!t)throw new Error("useMainInformation must be used within a LanguageProvider");return t};export{e as u};
--- assets/useMainInformation-BWY0MKEp.js
+++
@@ -1,1 +0,0 @@
-import{r as n,M as o}from"./index-GevSX3EX.js";const e=()=>{const t=n.useContext(o);if(!t)throw new Error("useMainInformation must be used within a LanguageProvider");return t};export{e as u};
