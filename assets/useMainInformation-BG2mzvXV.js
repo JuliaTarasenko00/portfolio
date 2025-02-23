e960a959f98b94477d8d72d5a31fa383c825d47e
JuliaTarasenko00 <118853187+JuliaTarasenko00@users.noreply.github.com>
2025-02-23 18:03:30 +0000

    Deploying to gh-pages from @ JuliaTarasenko00/portfolio@0889c355cee5e939ce5ccf25accd1938ed1092e2 🚀

--- assets/useMainInformation-BG2mzvXV.js
+++
@@ -1,1 +0,0 @@
-import{r as n,M as o}from"./index-B8HGPGj2.js";const e=()=>{const t=n.useContext(o);if(!t)throw new Error("useMainInformation must be used within a LanguageProvider");return t};export{e as u};
